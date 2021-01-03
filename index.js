42d7f2988701805ee991ab83f367655c4b6081fe
Mehtab Shadan <Mehtab.Shadan@logischtech.com>
2021-01-03 15:41:59 +0530

    Added Parser For Queries , Added Node mon for running app Continously

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
 var express = require('express');
+var bodyParser = require('body-parser')
 
 var app = express();
 var port  = 3000;
+/**
+ * MiddleWare For App
+ */
+app.use(bodyParser.json());
 
+//Routes
 app.get('/',(req,res,next)=> {
     res.send("Hello World")
 })
+
+app.post('/getParams' , (req,res,next)=>{
+    console.log(req.body);
+    console.log(req.params);
+    console.log(req.query);
+    res.sendStatus(200)
+})
+
+
 
 app.listen(port , () => {
     console.log(`Server Started at http://localhost:${port}`);
