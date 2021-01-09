47e6840e497b94e417ae867b1bc7aca3a24f210a
Mehtab Shadan <Mehtab.Shadan@logischtech.com>
2021-01-10 01:41:29 +0530

    Added Env and Clean Code

--- index.js
+++ index.js
@@ -1,32 +1,31 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-const postRouter = require('./Routes/posts-controller');
-const { startDbConnection, closeDbConnection } = require('./util/db-connect');
+const postRouter = require('./routes/posts-controller');
+require('dotenv/config')
+
 
 var app = express();
-var port  = 3000;
+var port = process.env.port || 3000;
+var host = process.env.host || 'localhost';
 /**
  * MiddleWare For App
  */
 app.use(bodyParser.json());
 
 //Routes 
-app.get('/', async (req,res,next)  => {
-    res.send("Hello World")
-    await startDbConnection();
+app.get('/', async (req, res, next) => {
+    res.send("Hello World");
 })
 // Post Routes 
-app.use('/posts',postRouter);
+app.use('/posts', postRouter);
 
-app.post('/getParams' , (req,res,next)=>{
+app.post('/getParams', (req, res, next) => {
     console.log(req.body);
     console.log(req.params);
     console.log(req.query);
     res.sendStatus(200)
 })
 
-
-
-app.listen(port , () => {
-    console.log(`Server Started at http://localhost:${port}`);
+app.listen(port, () => {
+    console.log(`Server Started at http://${host}:${port}`);
 })
--- util/db-connect.js
+++ util/db-connect.js
@@ -1,21 +1,12 @@
-const MongoClient = require('mongoose');
+require('dotenv/config')
+const {MongoClient} = require('mongodb');
+const uri = process.env.DB_CONNECTION;
+const myClient = new MongoClient(uri ,  { useNewUrlParser: true, useUnifiedTopology: true });
 
-exports.startDbConnection = async function () {
-  console.clear()
-  // console.log("in Connection");
+exports.client = myClient.connect();
 
-  const uri = "mongodb+srv://admin:jumpyAn!mal3@marstech.gcblu.mongodb.net/MarsTech?retryWrites=true&w=majority";
-  
-  await MongoClient.connect(uri, { useNewUrlParser: true , useUnifiedTopology : true} , (err,client)=>{
-    // console.log("Connection Successfully")
-    console.log(err);
-    return client;
-  });  
-}
 
-exports.closeDbConnection = function (connection) {
-  connection.close((err) => {
-    if (err) {return console.log(`Database Connection Closing Failed :: ${err.message}`) }
-    console.log("Database Connection Closed");
-  })
+
+exports.closeDbConnection = function () {
+  myClient.close();
 }
